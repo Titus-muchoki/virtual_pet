9b1f8ea94fbbb0b868d756b075bb2667cc3ad831
kajela <titoyut56@gmail.com>
2022-10-26 04:07:42 -0400

    add the initial files

---
+++ src/main/java/org/example/DB.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import org.sql2o.Sql2o;
+
+public class DB {
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets", "kajela", "8444");
+}
---
+++ src/main/java/org/example/Monster.java
@@ -0,0 +1,75 @@
+package org.example;
+
+import org.sql2o.Connection;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Monster {
+    private String name;
+    private int personId;
+
+    private int id;
+    private int foodLevel;
+    private int sleepLevel;
+    private int playLevel;
+
+    public static final int MAX_FOOD_LEVEL = 3;
+    public  static final int MAX_SLEEP_LEVEL = 8;
+    public static final int MAX_PLAY_LEVEL = 12;
+    public static final int MAX_ALL_LEVELS = 0;
+    public Monster(String name, int personId) {
+        this.name = name;
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Monster monster)) return false;
+        return personId == monster.personId && Objects.equals(name, monster.name);
+    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, personId);
+//    }
+public void save() {
+    try(Connection con = DB.sql2o.open()) {
+        String sql = "INSERT INTO monsters (name, personid) VALUES (:name, :personId)";
+        this.id = (int) con.createQuery(sql, true)
+                .addParameter("name", this.name)
+                .addParameter("personId", this.personId)
+                .executeUpdate()
+                .getKey();
+    }
+}
+
+    public int getId() {
+        return id;
+    }
+    public static List<Monster> all() {
+        String sql = "SELECT * FROM monsters";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Monster.class);
+        }
+    }
+    public static Monster find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM monsters where id=:id";
+            Monster monster = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Monster.class);
+            return monster;
+        }
+    }
+
+}
